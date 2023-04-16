ae06f8b2016cc14dd4ce872169a07f117d57e22d
AkshayChaudharyctrl <124782758+AkshayChaudharyctrl@users.noreply.github.com>
2023-04-16 10:59:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function getFormvalue() {
-    //Write your code here
-	 var firstName = document.getElementById("fname").value; 
-            var lastName= document.getElementById("lname").value;
-            var fullName= `${firstName} ${lastName}`;
-             alert(fullName);
+function AlertFunction(){
+	var x1 = document.getElementById('text1').value;
+	var x2 =  document.getElementById('text2').value;
+	var x3 =  document.getElementById('num').value;
+	var x4 =  document.getElementById('mail').value;
+	
+	alert("First Name: "+x1+"\n Last Name: "+x2+"\n Phone Number: "+x3+"\n Email ID: "+x4);
 }
